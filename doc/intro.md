b110054dd2f3c5f816f1c66026e5294dcfe41aac
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-04-27 22:25:38 -0700

    lein new rustic-kitchen

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rustic-kitchen
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rustic_kitchen/core_test.clj
@@ -0,0 +1,7 @@
+(ns rustic-kitchen.core-test
+  (:require [clojure.test :refer :all]
+            [rustic-kitchen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
